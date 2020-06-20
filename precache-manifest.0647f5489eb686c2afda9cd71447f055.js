7f26b0c5d56b88443f10997837705821ffdcd76f
ArtemBaskal <a.baskal@adguard.com>
2020-06-21 01:06:31 +0300

    Updates

---
+++ precache-manifest.0647f5489eb686c2afda9cd71447f055.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650b07be50160a6e7d02b7caca431e86",
+    "url": "/github-dashboard/index.html"
+  },
+  {
+    "revision": "129c53526e59a6edae3a",
+    "url": "/github-dashboard/static/css/3.e32dc58a.chunk.css"
+  },
+  {
+    "revision": "e3f4a5bcee4ec845ac51",
+    "url": "/github-dashboard/static/css/4.16d26523.chunk.css"
+  },
+  {
+    "revision": "93b432ea2cddc940dadd",
+    "url": "/github-dashboard/static/css/5.a07cf57d.chunk.css"
+  },
+  {
+    "revision": "f5cbf581893174a5af12",
+    "url": "/github-dashboard/static/css/main.27eae457.chunk.css"
+  },
+  {
+    "revision": "224ceed42e182aebfaf3",
+    "url": "/github-dashboard/static/js/2.e5ede334.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-dashboard/static/js/2.e5ede334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129c53526e59a6edae3a",
+    "url": "/github-dashboard/static/js/3.9d0c1c4d.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/github-dashboard/static/js/3.9d0c1c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f4a5bcee4ec845ac51",
+    "url": "/github-dashboard/static/js/4.de9e6268.chunk.js"
+  },
+  {
+    "revision": "93b432ea2cddc940dadd",
+    "url": "/github-dashboard/static/js/5.57282d3d.chunk.js"
+  },
+  {
+    "revision": "f5cbf581893174a5af12",
+    "url": "/github-dashboard/static/js/main.2b8d813a.chunk.js"
+  },
+  {
+    "revision": "9ec106f61044f5dff11f",
+    "url": "/github-dashboard/static/js/runtime-main.e9d868be.js"
+  }
+]);
