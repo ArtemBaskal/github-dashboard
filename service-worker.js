c3c7a68594851fea2cd03b2de811a4c530c11508
ArtemBaskal <a.baskal@adguard.com>
2020-06-20 14:15:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-dashboard/precache-manifest.c7822e1552163290f149c0d84cc1b738.js"
+  "/github-dashboard/precache-manifest.6708f096eda53fb2fbe651c3c611768b.js"
 );
 
 self.addEventListener('message', (event) => {
